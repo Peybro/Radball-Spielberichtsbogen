bc01ec016a6bf75b8d02ff17a1dd03d0318de78d
Thomas Kretschmann <t.kretschmann@t-online.de>
2022-12-12 12:51:47 +0100

    removed store for now

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
   import Accordion from "./lib/Accordion.svelte";
   import Team from "./model/Team";
-
-  import { count } from "./stores/store";
-
+  
   let data = {
     title: "",
     location: "",
@@ -21,19 +19,9 @@
     new Team("Team3"),
     new Team("Team4"),
   ];
-
-  let countValue;
-
-  count.subscribe((value) => {
-    countValue = value;
-  });
 </script>
 
 <main>
-  {countValue}
-  <button on:click={() => count.update((c) => c + 1)}>+</button>
-  <button on:click={() => count.update((c) => c - 1)}>-</button>
-  <button on:click={() => count.set(0)}>reset</button>
   <div class="container my-4">
     <Accordion bind:data bind:teams />
   </div>
