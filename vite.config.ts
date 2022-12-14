73ac7be0aa6ba02e44b4f756f1733216738c7ad7
Thomas Kretschmann <t.kretschmann@t-online.de>
2022-12-14 16:33:20 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
         theme_color: "#111111",
         background_color: "#3367D6",
         display: "standalone",
-        scope: "/",
+        scope: "/spiel/",
         description: "Manager für Radball Spieltage",
       },
     }),
