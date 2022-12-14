d6347d484fbec0b07e2d0ed25d575f762f654c1d
Thomas Kretschmann <t.kretschmann@t-online.de>
2022-12-14 02:57:38 +0100

    added missing $'s

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
-    import type {data} from "../model/types";
-
-    export let metaInfo: data;
+    import {metaInfo} from "./../stores/store"
 
     function setDate(): void {
         const today = new Date();
-        metaInfo.date = `${today.getFullYear()}-${
+        $metaInfo.date = `${today.getFullYear()}-${
             today.getMonth() + 1
         }-${today.getDate()}`;
     }
 
     function setStartTime(): void {
         const today = new Date();
-        metaInfo.startTime =
+        $metaInfo.startTime =
             `0${today.getHours()}`.slice(-2) +
             ":" +
             `0${today.getMinutes()}`.slice(-2);
@@ -20,7 +18,7 @@
 
     function setEndTime(): void {
         const today = new Date();
-        metaInfo.endTime =
+        $metaInfo.endTime =
             `0${today.getHours()}`.slice(-2) +
             ":" +
             `0${today.getMinutes()}`.slice(-2);
@@ -30,7 +28,7 @@
 <div class="mb-3">
     <label class="form-label" for="titleInput">Spielberichtsbogen</label>
     <input
-            bind:value={metaInfo.title}
+            bind:value={$metaInfo.title}
             class="form-control"
             id="titleInput"
             placeholder="Veranstaltungstitel"
@@ -40,7 +38,7 @@
 <div class="mb-3">
     <label class="form-label" for="locationTextarea">Spielort</label>
     <textarea
-            bind:value={metaInfo.location}
+            bind:value={$metaInfo.location}
             class="form-control"
             id="locationTextarea"
             placeholder="Anschrift Veranstaltungsort"
@@ -54,7 +52,7 @@
             <label class="form-label" for="dateInput" on:click={setDate} on:keydown={setDate}
             >Datum</label
             >
-            <input bind:value={metaInfo.date} class="form-control" id="dateInput" type="date"/>
+            <input bind:value={$metaInfo.date} class="form-control" id="dateInput" type="date"/>
         </div>
     </div>
     <div class="col-sm-4 col-xs-12">
@@ -65,7 +63,7 @@
                     on:click={setStartTime}
                     on:keydown={setStartTime}>Beginn</label
             >
-            <input bind:value={metaInfo.startTime} class="form-control" id="startTimeInput" type="time"/>
+            <input bind:value={$metaInfo.startTime} class="form-control" id="startTimeInput" type="time"/>
         </div>
     </div>
     <div class="col-sm-4 col-xs-12">
@@ -76,7 +74,7 @@
                     on:click={setEndTime}
                     on:keydown={setEndTime}>Ende</label
             >
-            <input bind:value={metaInfo.endTime} class="form-control" id="endTimeInput" type="time"/>
+            <input bind:value={$metaInfo.endTime} class="form-control" id="endTimeInput" type="time"/>
         </div>
     </div>
 </div>
@@ -85,19 +83,19 @@
     <div class="col-sm-4 col-xs-12">
         <div class="mb-3">
             <label class="form-label" for="chiefRefereeInput">Chief-Kommisär</label>
-            <input bind:value={metaInfo.chiefReferee} class="form-control" id="chiefRefereeInput" type="text"/>
+            <input bind:value={$metaInfo.chiefReferee} class="form-control" id="chiefRefereeInput" type="text"/>
         </div>
     </div>
     <div class="col-sm-4 col-xs-12">
         <div class="mb-3">
             <label class="form-label" for="refereeInput">Kommissär</label>
-            <input bind:value={metaInfo.referee} class="form-control" id="refereeInput" type="text"/>
+            <input bind:value={$metaInfo.referee} class="form-control" id="refereeInput" type="text"/>
         </div>
     </div>
     <div class="col-sm-4 col-xs-12">
         <label class="form-label" for="durationInput">Spielzeit</label>
         <div class="input-group mb-3">
-            <input bind:value={metaInfo.duration} class="form-control" id="durationInput" type="text"/>
+            <input bind:value={$metaInfo.duration} class="form-control" id="durationInput" type="text"/>
             <span class="input-group-text">Minuten</span>
         </div>
     </div>
@@ -106,7 +104,7 @@
 <div class="mb-3">
     <label class="form-label" for="notPlayingTextarea">Spielfrei</label>
     <textarea
-            bind:value={metaInfo.notPlaying}
+            bind:value={$metaInfo.notPlaying}
             class="form-control"
             id="notPlayingTextarea"
             placeholder="Spielfrei"
