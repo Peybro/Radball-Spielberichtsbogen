e1e2a19eca8687b83e48057b563772eca376dd78
Thomas Kretschmann <t.kretschmann@t-online.de>
2022-12-14 16:06:21 +0100

    properly (hopefully) implemented vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: [
+        "favicon.png",
+        "apple-touch-icon.png",
+        "vite.svg",
+        "android-chrome-192x192.png",
+        "android-chrome-512x512.png",
+      ],
+      manifest: {
+        short_name: "Spielmanager",
+        name: "Spielberichtsbogen",
+        start_url: "/",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#111111",
+        background_color: "#3367D6",
+        display: "standalone",
+        scope: "/",
+        description: "Manager für Radball Spieltage",
+      },
+    }),
+  ],
 });
